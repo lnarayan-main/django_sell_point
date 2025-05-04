7fb9cc1bf11d00c0b193e46d1000a3c63a59b8f6
lnarayan-main <laxmi.narayan.main1@gmail.com>
2025-05-04 15:28:51 +0530

    fixes

--- homepage/models.py
+++ homepage/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -7,6 +8,7 @@ class Category(models.Model):
     parent = models.ForeignKey('self', null=True, blank=True, related_name='children', on_delete=models.SET_NULL, verbose_name="Parent Category")
     status = models.BooleanField(default=True, verbose_name="Status")
     image = models.ImageField(upload_to='category_pics/', null=True, blank=True)
+    slug = models.SlugField(null=True, blank=True) 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -14,6 +16,12 @@ class Category(models.Model):
         verbose_name = "Category"
         verbose_name_plural = "Categories"
         ordering = ['name']
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            # Convert name to lowercase with underscores
+            self.slug = slugify(self.name).replace('-', '_')
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
--- homepage/views.py
+++ homepage/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from core.decorators import admin_required, logged_in_required
+from homepage.models import Category
+from products.models import Product
+from itertools import zip_longest
+
+def chunked(iterable, n):
+    args = [iter(iterable)] * n
+    return zip_longest(*args)
 
 # Create your views here.
 
 # @logged_in_required
 # @admin_required
 def home(request):
-    return render(request, 'homepage/home.html')
+    categories = Category.objects.all()
+    products = Product.objects.all()
+    grouped = list(chunked(products, 3))  
+    context = {
+        'categories' : categories,
+        'products' : products,
+        'grouped_products' : grouped
+    }
+    return render(request, 'homepage/home.html', context)
 
 def register(request):
     return render(request, 'homepage/register.html')
